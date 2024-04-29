47105a6f84f6f73f6bfa9f21323aad63ef053787
Jayce <ashishalex084@gmail.com>
2024-04-29 20:36:36 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Hero from './Hero'
+import Articles from './Articles'
+
+const App = () => {
+  return (
+
+    <div >
+    <Hero />
+    <Articles />
+  </div>
+  )
+}
+
+export default App
