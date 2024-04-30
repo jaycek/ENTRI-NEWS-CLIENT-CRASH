6e7a0c47b2acb7eb57fbfbcbe4c55f54ecbacedf
Jayce <ashishalex084@gmail.com>
2024-04-30 21:49:03 +0530

    updated image path

--- src/Articles.jsx
+++ src/Articles.jsx
@@ -1,22 +1,24 @@
 import React from 'react'
 import Article from './Article'
+import imageretro from './assets/images/image-retro-pcs.jpg'
+import imagelaptop from 'src/assets/images/image-top-laptops.jpg'
 
 const Articles = () => {
   return (
     <section className='articles'>
         <Article 
         id='01' title='Reviving Retro PCs' desc=' What happens when old PCs are given modern upgrades?' 
-        img='src/assets/images/image-retro-pcs.jpg'
+        img={imageretro}
         
         />
          <Article 
         id='02' title='Top 10 Laptops of 2022' desc=' Our best picks for various needs and budgets' 
-        img='src/assets/images/image-top-laptops.jpg'
+        img={imagelaptop}
         
         />
          <Article 
         id='03' title='The Growth of Gaming' desc='How the pandemic has sparked fresh opportunities.'
-         img='src/assets/images/image-retro-pcs.jpg'
+         img={imageretro}
         
         />
       
